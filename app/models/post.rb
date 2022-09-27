6a61d8a86f9e68653675c696fdbda9bec09855e4
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-09-27 19:29:03 +0300

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+    validates :title, presence: true 
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction']}
+
+    validate :clickbait?
+
+    def clickbait?
+        patterns = [/Won't Believe/, /Secret/, /Top \d/, /Guess/]
+
+        if patterns.none? { |pattern| pattern.match(self.title) }
+             errors.add(:title, "must be a clickbait-y title")
+        end
+    end
 end
+
